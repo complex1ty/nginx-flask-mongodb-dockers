551f3b9d9bbf5bcd5a227bf5c294db56e7dfbf83
Fu Qiang <complexonety@gmail.com>
2018-12-06 00:36:31 +0800

    connect flask with mongodb

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from pymongo import MongoClient
+from random import randint
 
 app = Flask(__name__)
+client = MongoClient(
+    "mongodb://mongodb:27017"
+)
+db = client.testdb
 
-@app.route('/')
+@app.route('/test')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/api')
+def insert_mongo_db():
+    rand = randint(0, 100)
+    item = {'number': randint(0, 100)}
+    db.test_collection.insert_one(item)
+    return 'Insert ' + str(rand) + ' into MongoDB!'
+
+@app.route('/api/get')
+def get_mongo_db():
+    rand = db.test_collection.find_one()['number']
+    return 'Get ' + str(rand) + ' from MongoDB!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
